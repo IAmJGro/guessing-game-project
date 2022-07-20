95c5b6b03aaa5fabd37e661129ba14bc5357c587
Jasper Groner <jasper.groner@gmail.com>
2022-07-19 21:51:25 -0400

    Add additional dynamic functionality
    Make range non-static with randomInRange (). Allow user to input range
    with askRange(). Set limit to number of guesses from user. Allow user to
    change the number of guesses.

--- guessing-game.js
+++ guessing-game.js
@@ -4,8 +4,74 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-askGuess();
 let secretNumber = 7;
+let numAttempts = 5;
+
+askLimit();
+
+function askLimit()
+{
+    rl.question("Enter the number of guesses: ", answer => {
+        numAttempts = Number(answer);
+        askRange();
+    });
+}
+
+function askRange()
+{
+    let min = 0;
+    let max = 10;
+    rl.question("Enter a max number: ", getMax);
+    // assign input maximum as end of range
+    function getMax(answer)
+    {
+        max = answer;
+        rl.question("Enter a min number: ", getMin);
+    }
+    // assign input minimum to beginning of range
+    function getMin(answer)
+    {
+        min = answer;
+        startGame();
+    }
+    // tell user range, get a secret number and ask for guesses
+    function startGame()
+    {
+        console.log(`I'm thinking of an number between ${min} and ${max}...`);
+        min = Number(min);
+        max = Number(max);
+        secretNumber = randomInRange(min, max);
+        askGuess();
+    }
+}
+
+
+function askGuess()
+{
+    // decrement # of attempts
+    console.log(`Attempts remaining: ${numAttempts}`);
+    numAttempts--;
+
+    // recursively calls itself if you don't get the answer right
+    rl.question("Enter a guess: ", (answer) => {
+        answer = Number(answer);
+        if (checkGuess(answer))
+        {
+            console.log("You win!");
+            rl.close();
+        }
+        else if (numAttempts > 0)
+        {
+            askGuess();
+        }
+        else
+        {
+            console.log("You lose.");
+            rl.close();
+        }
+    });
+}
+
 
 function checkGuess(guess)
 {
@@ -29,18 +95,11 @@ function checkGuess(guess)
     return false;
 }
 
-function askGuess()
+function randomInRange(min, max)
 {
-    rl.question("Enter a guess: ", (answer) => {
-        answer = Number(answer);
-        if (checkGuess(answer))
-        {
-            console.log("You win!");
-            rl.close();
-        }
-        else
-        {
-            askGuess();
-        }
-    });
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    // because we are flooring, we want the range to be one grater than max - min
+    // otherwise can never have max as random result
+    return Math.floor(Math.random() * (max + 1 - min) + min);
 }
